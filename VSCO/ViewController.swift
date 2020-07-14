19806fae74f44195ba5c2e1d308291f460b43267
Eduard <51046544+pressanykeyplease@users.noreply.github.com>
2020-07-15 00:18:44 +0300

    Initial Commit

---
+++ VSCO/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VSCO
+//
+//  Created by Eduard Mukhametgareev on 15.07.2020.
+//  Copyright © 2020 Eduard Mukhametgareev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
